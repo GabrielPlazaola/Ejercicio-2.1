a65c4b3514183f65afe79f197b03275f8d34c3d4
Gabriel <70722542+GabrielPlazaola@users.noreply.github.com>
2023-05-20 00:47:02 -0300

    .

---
+++ mainpag.js
@@ -0,0 +1,38 @@
+const correo = document.getElementById('correos');
+const templateCorreo = document.getElementById('template-correo').content;
+const fragment = document.createDocumentFragment();
+
+function cambiarColor(botonPresionado) {
+  var botones = document.querySelectorAll('.boton-cambio');
+  botones.forEach(function(boton) {
+    if (boton !== botonPresionado) {
+      boton.classList.remove('bg-red-500');
+    }
+  });
+  botonPresionado.classList.toggle('bg-red-500');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchData();
+});
+
+const fetchData = async () => {
+  try {
+    const res = await fetch('../api.json');
+    const data = await res.json();
+    pintarCorreos(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const pintarCorreos = data => {
+  data.forEach(correo => {
+    templateCorreo.querySelector(".remitente").textContent = correo.correo;
+    templateCorreo.querySelector(".Asunto").textContent = correo.asunto;
+    templateCorreo.querySelector(".cuerpo").textContent = correo.mensaje.split(' ').slice(0, 20).join(' ');
+    const clone = templateCorreo.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  correo.appendChild(fragment);
+};
